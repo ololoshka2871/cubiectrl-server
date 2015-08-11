b47af42c435873f01f827b736da8db4cb211a02f
shiloxyz <shiloxyz@q6600arch>
2015-08-11 19:42:46 +0300

    big display plaing ctrl

--- displayControl.go
+++ displayControl.go
@@ -22,7 +22,9 @@ type tCurrentDisplayState struct {
 	SmallDisplayMode bool
 	BigDisplayMode int
 	
-	SmallDisplayPlayerProcess *os.Process
+	SmallDisplayPlayerProcess 	*os.Process
+	BigDisplayPlayerProcess 	*os.Process
+	BigDisplayValuesProcess		*os.Process
 }
 
 var CurrentDisplayState tCurrentDisplayState
@@ -35,7 +37,7 @@ func StartDefault() {
 func ControlSmallDisplay(enable bool) error {
 	if enable != CurrentDisplayState.SmallDisplayMode {
 		if enable {
-			env := append(os.Environ(), Big_Display) // set DISPLAY env
+			env := append(os.Environ(), Small_Display) // set DISPLAY env
 			var PlayerArgs []string
 			if media, ok := settings.Value("SmallDispFileName", "").(string); !ok || media == "" {
 				return errors.New("Playing media error")
@@ -45,7 +47,7 @@ func ControlSmallDisplay(enable bool) error {
 			if proc, err :=	os.StartProcess(Player, PlayerArgs, &os.ProcAttr{Env : env}); err == nil {
 				CurrentDisplayState.SmallDisplayPlayerProcess = proc
 			} else {
-				log.Println("Failed to start paing")
+				log.Println("Failed to start plaing on SMALL display")
 				return err
 			}
 		} else {
@@ -71,7 +73,62 @@ func ControlSmallDisplay(enable bool) error {
 
 func ControlBigDisplay(ctrl int) error {
 	if ctrl != CurrentDisplayState.BigDisplayMode {
-		//
+		env := append(os.Environ(), Big_Display) // set DISPLAY env
+		switch(ctrl) {
+			case Diable_bigDisplay:
+				if CurrentDisplayState.BigDisplayPlayerProcess != nil {
+					CurrentDisplayState.BigDisplayPlayerProcess.Kill()
+					CurrentDisplayState.BigDisplayPlayerProcess = nil
+				}
+				if CurrentDisplayState.BigDisplayValuesProcess != nil {
+					CurrentDisplayState.BigDisplayValuesProcess.Kill()
+					CurrentDisplayState.BigDisplayValuesProcess = nil
+				}
+				log.Println("Big Display disabled")
+				return nil
+				
+			case ShowVideo_bigDisplay:
+				if CurrentDisplayState.BigDisplayValuesProcess != nil {
+					CurrentDisplayState.BigDisplayValuesProcess.Kill()
+					CurrentDisplayState.BigDisplayValuesProcess = nil
+				}
+				if CurrentDisplayState.BigDisplayPlayerProcess != nil {
+					CurrentDisplayState.BigDisplayMode = ShowVideo_bigDisplay
+					return errors.New("Allready playing")
+				}
+				
+				var PlayerArgs []string
+				if media, ok := settings.Value("BigDispFileName", "").(string); !ok || media == "" {
+					return errors.New("Playing media error")
+				} else {
+					PlayerArgs = append(PlayerArgsCommon, media)
+				}
+				if proc, err :=	os.StartProcess(Player, PlayerArgs, &os.ProcAttr{Env : env}); err == nil {
+					CurrentDisplayState.BigDisplayPlayerProcess = proc
+				} else {
+					log.Println("Failed to start plaing on BIG display")
+					return err
+				}
+				
+				log.Println("Start playing on small display")
+				return nil
+				
+			case ShowQMLForm_bigDisplay:
+				if CurrentDisplayState.BigDisplayPlayerProcess != nil {
+					CurrentDisplayState.BigDisplayPlayerProcess.Kill()
+					CurrentDisplayState.BigDisplayPlayerProcess = nil
+				}
+				if CurrentDisplayState.BigDisplayValuesProcess != nil {
+					CurrentDisplayState.BigDisplayMode = ShowQMLForm_bigDisplay
+					return errors.New("Allready Displaying")
+				}
+				
+				log.Println("Displaing values on big display")
+				return nil
+				
+			default :
+				return errors.New("Incorrect ctrl request")
+		}
 		
 		CurrentDisplayState.BigDisplayMode = ctrl
 	}
