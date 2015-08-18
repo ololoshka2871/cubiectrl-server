2b9afaefbcda94fb641d6441a49693ecc0ce2faf
ololoshka2871 <SweetTreasure@2ch.hk>
2015-08-18 15:30:10 +0300

    disable switch mode while sleep

--- modeSwitcher.go
+++ modeSwitcher.go
@@ -35,7 +35,7 @@ func PrepareModeSwitcher(pin string) error {
 							log.Println("Switch mode button")
 							switch CurrentDisplayState.BigDisplayMode {
 								case Diable_bigDisplay:
-									ControlBigDisplay(ShowVideo_bigDisplay)
+									return
 								case ShowVideo_bigDisplay:
 									ControlBigDisplay(ShowQMLForm_bigDisplay)
 								case ShowQMLForm_bigDisplay:
